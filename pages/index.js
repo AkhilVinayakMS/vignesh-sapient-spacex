86d682ba821c3daf6ebc617ccf0d74829aa6f549
vigneshbalaji07 <Vignesh@satetechnologies.com>
2020-10-19 15:23:35 +0530

    added loader

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,17 @@ function Home(props) {
   const { land_success, launch_success, launch_year } = router.query;
   const [filter, setFilter] = useState({year:launch_year ? launch_year:'',launch:launch_success?launch_success:'',landing:land_success?land_success:''});
   const [flights, setFlights] = useState(launches);
+  const [loading, setLoading] = useState(false);
  
 useEffect(() => {
     const yearFilter = launch_year ? launch_year : ""
     const launchFilter = launch_success ? launch_success : ""
-    const landingFilter = land_success ? land_success : ""
+  const landingFilter = land_success ? land_success : ""
+  setLoading(true)
     axios.get(`https://api.spacexdata.com/v3/launches?limit=100&launch_year=${yearFilter}&launch_success=${launchFilter}&land_success=${landingFilter}`)
       .then(res => {
         setFlights(res.data)
+        setLoading(false);
       })
   }, [land_success,launch_success,launch_year]);
 
@@ -67,6 +70,9 @@ useEffect(() => {
               />
               
             </Col>)}
+
+            {loading && <Col><h4>Loading...........</h4></Col>}
+            {!loading && flights.length === 0 && <Col><h4>No Data</h4></Col>}
             
           </Row>
         </Col>
